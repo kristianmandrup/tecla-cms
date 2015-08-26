fc071e64abf95bf2b9ae9a99ebabfb2c5eab4f7f
Ankush K <katariyaankush574@gmail.com>
2015-08-26 19:56:51 +0530

    Typo issue

--- app/models/cms/hash_list.rb
+++ app/models/cms/hash_list.rb
@@ -11,13 +11,13 @@ class Cms::HashList < Cms::CompositeHashItem
       if item._type == "Cms::HashList"
         hash[item.name] = item.set_hash
       elsif item._type == "Cms::BlockList"
-        hash[item.name] = item.blocks.collect{ |block| block.as_json(only: [:name])}
+        hash[item.name] = item.blocks.collect{ |block| block.as_json(only: [:title])}
       elsif item._type == "Cms::ImageList"
-        hash[item.name] = item.images.collect{ |block| block.as_json(only: [:name])}
+        hash[item.name] = item.images.collect{ |image| image.as_json(only: [:title])}
       elsif item._type == "Cms::NamedBlock"
-        hash[item.name] = item.block.as_json(only: [:name])
+        hash[item.name] = item.block.as_json(only: [:title])
       elsif item._type == "Cms::NamedImage"
-        hash[item.name] = item.image.as_json(only: [:name])
+        hash[item.name] = item.image.as_json(only: [:title])
       end
     end
     hash
