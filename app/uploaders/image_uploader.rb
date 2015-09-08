bc2e05628bca360cbd93bb68c039384d7b901f6b
Ankush K <katariyaankush574@gmail.com>
2015-09-08 18:14:07 +0530

    Made some changes to imageuploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -17,15 +17,15 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   # Create different versions of uploaded files:
-  version :large_avatar do
+  version :large do
     # returns a 150x150 image
     process :resize_to_fill => [150, 150]
   end
-  version :medium_avatar do
+  version :medium do
     # returns a 50x50 image
     process :resize_to_fill => [50, 50]
   end
-  version :small_avatar do
+  version :thumbnail do
     # returns a 35x35 image
     process :resize_to_fill => [35, 35]
   end
