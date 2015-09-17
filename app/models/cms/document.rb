61268c09f4bfa21d0bf31f3cbd2ab488d750b18e
Kristian Mandrup <kmandrup@gmail.com>
2015-09-17 22:54:35 +0200

    document fails if I add orderable

--- app/models/cms/document.rb
+++ app/models/cms/document.rb
@@ -1,4 +1,9 @@
 class Cms::Document
-  include Concerned
-  include_concerns :document
+  # include Concerned
+  # include_concerns :document #, :listable
+  include Mongoid::Document
+  include Mongoid::Orderable
+  include Named
+  # orderable
+
 end
