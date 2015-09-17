17206a31bf69a666cb3665479e890ca254da2e68
Kristian Mandrup <kmandrup@gmail.com>
2015-09-17 22:35:36 +0200

    block spec passes

--- app/models/concerns/describable.rb
+++ app/models/concerns/describable.rb
@@ -3,6 +3,6 @@ module Describable
 
   included do
     # used for authors to descibe what is being edited, not for display
-    field :description,        type: String, localize: true
+    field :description, type: String, localize: true
   end
 end
--- app/models/concerns/renderable.rb
+++ app/models/concerns/renderable.rb
@@ -1,24 +1,21 @@
 module Renderable
+  extend ActiveSupport::Concern
+
   # Usage block.render template: template, layout: layout
   def render opts = {}
     renderer.render opts
   end
 
-  # is this the Template handler to be used for rendering?
-  def handler template
-    Liquid::Template.parse template || @template
-  end
-
   protected
 
   # must have a template and layout method or field
-  def validate!
-    throw 'Not a valid renderable' unless valid?
+  def validate_render!
+    throw 'Not a valid renderable' unless valid_render?
   end
 
-  #def valid?(context = nil)
-  #  [:template, :layout].each {|method| self.respond_to? method }
-  #end
+  def valid_render?
+   [:template, :layout].each {|method| self.respond_to? method }
+  end
 
   # can we cache it?
   def renderer
