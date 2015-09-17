bdd096e51881719d369465def2b5599ecd489b9d
Ankush K <katariyaankush574@gmail.com>
2015-09-18 02:14:03 +0530

    fixed validations specs

--- spec/factories/cms/image.rb
+++ spec/factories/cms/image.rb
@@ -3,6 +3,7 @@
 FactoryGirl.define do
   factory :image, class: "Cms::Image" do
     title { Faker::Name.name }
+    name { Faker::Name.name }
     content { Rack::Test::UploadedFile.new(File.join(Rails.root, 'public', 'a2.jpg')) }
   end
 end
--- spec/models/cms/template/template_finder_spec.rb
+++ spec/models/cms/template/template_finder_spec.rb
@@ -3,21 +3,24 @@ require 'rails_helper'
 RSpec.describe Cms::Template::TemplateFinder, type: :model do
  
   let(:template_finder) { Cms::Template::TemplateFinder.instance }
+  after(:all) do
+    Cms::Template.destroy_all
+  end
 
   it "should find template" do
-    template1 = FactoryGirl.create(:template, name: 'my_template1')
+    template1 = FactoryGirl.create(:template, name: Faker::Name.name )
     template2 =  template_finder.find_template(template1.name)
     expect(template2.name).to eq(template1.name)
   end
   
   it "should find layout" do
-    layout = FactoryGirl.create(:template, name: 'my_layout1', type: "layout")
+    layout = FactoryGirl.create(:template, name: Faker::Name.name , type: "layout")
     template =  template_finder.find_layout(layout.name)
     expect(template.name).to eq(layout.name)
   end
   
   it "should find partial" do
-    partial = FactoryGirl.create(:template, name: 'my_partial1', type: "partial")
+    partial = FactoryGirl.create(:template, name: Faker::Name.name, type: "partial")
     template =  template_finder.find_partial(partial.name)
     expect(template.name).to eq(partial.name)
   end
