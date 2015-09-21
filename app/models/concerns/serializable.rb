544b33862be29afdc1398d195b3441df5359d478
Ankush K <katariyaankush574@gmail.com>
2015-09-21 15:28:56 +0530

    fixed syntax error and bad name issue

--- app/models/concerns/serializable.rb
+++ app/models/concerns/serializable.rb
@@ -3,25 +3,26 @@ module Localizable
 
   included do
 
-  def as_json(options={})
-    super(:only => api_attributes, :methods => :type)
-  end
-
-  def api_attributes
-    @api_attributes ||= potential_api_attributes.filter do |name|
-      has_field? name
+    def as_json(options={})
+      super(:only => api_attributes, :methods => :type)
     end
-  end
 
-  def potential_api_attributes
-    default_api_attributes.merge(my_api_attributes)
-  end
+    def api_attributes
+      @api_attributes ||= potential_api_attributes.filter do |name|
+        has_field? name
+      end
+    end
 
-  def my_api_attributes
-    []
-  end
+    def potential_api_attributes
+      default_api_attributes.merge(my_api_attributes)
+    end
 
-  def default_api_attributes
-    [:title, :label, :link, :icon, :categories, :tags, :description, :content]
+    def my_api_attributes
+      []
+    end
+
+    def default_api_attributes
+      [:title, :label, :link, :icon, :categories, :tags, :description, :content]
+    end
   end
 end
