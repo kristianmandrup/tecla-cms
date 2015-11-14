f627be45e384c033774479c9bdde6a8e38a8c943
Kuldeep Aggarwal <kd.engineer@yahoo.co.in>
2015-11-14 22:32:58 +0530

    correct typo `supar_admin` ~> super_admin

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,21 +4,20 @@ FactoryGirl.define do
     password { 'password' }
     password_confirmation { 'password' }
 
-
-    factory :supar_admin do
-        after(:create) {|user| user.add_role(:supar_admin)}
+    factory :super_admin do
+      after(:create) { |user| user.add_role(:super_admin) }
     end
 
     factory :admin do
-        after(:create) {|user| user.add_role(:admin)}
+      after(:create) { |user| user.add_role(:admin) }
     end
 
     factory :publisher do
-        after(:create) {|user| user.add_role(:publisher)}
+      after(:create) { |user| user.add_role(:publisher) }
     end
 
     factory :author do
-        after(:create) {|user| user.add_role(:author)}
+      after(:create) { |user| user.add_role(:author) }
     end
   end
 end
